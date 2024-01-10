efe046505e7ff54155858b10601306f4be1f1b03
Alxsnc <alxsnc83@gmail.com>
2024-01-09 21:26:41 -0500

    cambios del login sin autentificacion

--- src/app/modules/login/login-routing.module.ts
+++ src/app/modules/login/login-routing.module.ts
@@ -1,14 +1,13 @@
 import { PublicacionComponent } from './pages/publicacion/publicacion.component';
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-//import { LoginComponent } from "./pages/login/login.component";
+
 import { RegistroComponent } from "./pages/registro/registro.component";
 import { Login2Component } from './pages/login2/login2.component';
 
 const rutas: Routes=[{
     path:'',children:[
-      {path:'login2',component:Login2Component},
-        //{path:'login',component:LoginComponent},
+        {path:'login2',component:Login2Component},
         {path:'registro',component:RegistroComponent},
         {path:'publicacion',component:PublicacionComponent},
 
