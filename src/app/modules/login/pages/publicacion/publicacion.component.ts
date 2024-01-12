ef1427f37a98c447b156e337e4c6791fc8e58f0b
Alxsnc <alxsnc83@gmail.com>
2024-01-12 11:02:31 -0500

    controles en publicaciones

--- src/app/modules/login/pages/publicacion/publicacion.component.ts
+++ src/app/modules/login/pages/publicacion/publicacion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -7,39 +8,41 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./publicacion.component.scss']
 })
 export class PublicacionComponent implements OnInit {
-
-  public myForm!:FormGroup;
+  public myForm!: FormGroup;
 
   constructor(
-    private fb:FormBuilder,
-    ) { }
+    private fb: FormBuilder,
+    private routerprd: Router
+  ) {}
 
   ngOnInit(): void {
-    this.myForm=this.createMyForm();
+    this.myForm = this.createMyForm();
   }
 
-  private createMyForm():FormGroup{
+  private createMyForm(): FormGroup {
     return this.fb.group({
-      titulo:['',[Validators.maxLength(100)]],
-      descripcion:['',[Validators.maxLength(500)]],
-      pago:['',[Validators.required,Validators.pattern(/^[0-9]+$/),Validators.maxLength(4)]],
-      ciudad:['',[Validators.required,Validators.pattern(/^[a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/),Validators.maxLength(20)]],
-      categoria:['',[Validators.required,Validators.pattern(/^[a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/),Validators.maxLength(20)]],
+      titulo: ['', [Validators.required, Validators.maxLength(100)]],
+      descripcion: ['', [Validators.required, Validators.maxLength(500)]],
+      pago: ['', [Validators.required, Validators.pattern(/^[0-9]+$/), Validators.maxLength(4)]],
+      ciudad: ['', [Validators.required, Validators.pattern(/^[a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/), Validators.maxLength(20)]],
+      categoria: ['', [Validators.required, Validators.pattern(/^[a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/), Validators.maxLength(20)]],
     });
   }
 
-  public submitFormulario(){
-
-    if(this.myForm.invalid){
-      Object.values(this.myForm.controls).forEach(control=>{control.markAllAsTouched()});
+  public submitFormulario(): void {
+    if (this.myForm.invalid) {
+      Object.values(this.myForm.controls).forEach(control => {
+        control.markAllAsTouched();
+      });
       return;
     }
 
+    // Aquí puedes agregar lógica adicional cuando el formulario es válido y se envía.
   }
 
-  public get f():any{
+  public get f(): any {
     return this.myForm.controls;
   }
 
-
+  
 }
