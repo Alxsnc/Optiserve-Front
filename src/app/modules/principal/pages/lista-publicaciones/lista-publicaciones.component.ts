0fb741ef14403fd93c2d2123052dc8517c504c5c
Anderson Santana <98790211+Juvth@users.noreply.github.com>
2024-01-29 08:51:35 -0500

    Updete mensajes en eliminar publicaciónes, muestra los mensajes que retorna el backend

--- src/app/modules/principal/pages/lista-publicaciones/lista-publicaciones.component.ts
+++ src/app/modules/principal/pages/lista-publicaciones/lista-publicaciones.component.ts
@@ -42,31 +42,74 @@ export class ListaPublicacionesComponent implements OnInit {
     )
   }
 
-  mostrarAlerta(publicacion: Publicaciones){
+  mostrarAlerta(publicacion: Publicaciones): void {
     Swal.fire({
       title: '¿Está seguro de borrar esta publicación?',
-      text: "¡Esta accion no se podra revertir!",
+      text: "¡Esta acción no se podrá revertir!",
       icon: 'warning',
       showCancelButton: true,
       confirmButtonColor: '#006666',
       cancelButtonColor: '#cc6666',
-      cancelButtonText:'Cancelar',
-      confirmButtonText: 'Si, quiero eliminarla!'
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'Sí, quiero eliminarla!'
     }).then((result) => {
       if (result.isConfirmed) {
-        Swal.fire({
-          title: 'Publicacion Borrada!',
-          text: 'La publicacion ha sido borrada con exito.',
-          icon: 'success',
-          confirmButtonColor: '#006666',
-          confirmButtonText: "Aceptar"
-        });
-        // Aquí puedes poner la lógica para eliminar los datos si el usuario hizo clic en "Aceptar"
-        this.publicacionService.eliminarPublicacion(publicacion).subscribe();
-        this.listaActivas();
+        // Hacer la solicitud HTTP para eliminar la publicación
+        this.publicacionService.eliminarPublicacion(publicacion).subscribe(
+          (response: any) => {
+            // Mostrar mensaje de éxito
+            Swal.fire({
+              title: 'Publicación Borrada!',
+              text: response.message, // Utilizar el mensaje proporcionado por el backend
+              icon: 'success',
+              confirmButtonColor: '#006666',
+              confirmButtonText: "Aceptar"
+            });
+
+            // Aquí puedes realizar cualquier otra acción después de eliminar con éxito
+            this.listaActivas();
+          },
+          (error: any) => {
+            // Mostrar mensaje de error
+            Swal.fire({
+              title: 'Error al borrar la publicación',
+              text: error.error.error || 'Su publicación tiene postulaciones en proceso, no puede ser eliminada.',
+              icon: 'error',
+              confirmButtonColor: '#cc6666',
+              confirmButtonText: "Aceptar"
+            });
+          }
+        );
       }
     });
   }
+
+
+  // mostrarAlerta(publicacion: Publicaciones){
+  //   Swal.fire({
+  //     title: '¿Está seguro de borrar esta publicación?',
+  //     text: "¡Esta accion no se podra revertir!",
+  //     icon: 'warning',
+  //     showCancelButton: true,
+  //     confirmButtonColor: '#006666',
+  //     cancelButtonColor: '#cc6666',
+  //     cancelButtonText:'Cancelar',
+  //     confirmButtonText: 'Si, quiero eliminarla!'
+  //   }).then((result) => {
+  //     if (result.isConfirmed) {
+  //       Swal.fire({
+  //         title: 'Publicacion Borrada!',
+  //         text: 'La publicacion ha sido borrada con exito.',
+  //         icon: 'success',
+  //         confirmButtonColor: '#006666',
+  //         confirmButtonText: "Aceptar"
+  //       });
+  //       // Aquí puedes poner la lógica para eliminar los datos si el usuario hizo clic en "Aceptar"
+  //       this.publicacionService.eliminarPublicacion(publicacion).subscribe();
+  //       this.listaActivas();
+  //     }
+  //   });
+  // }
 
 
 
